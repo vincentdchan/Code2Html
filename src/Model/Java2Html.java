5558f7be1550cc9b70a8747f85245fe02afa6033
ChannelOne <cdzos97@gmail.com>
2017-03-11 01:16:52 +0800

    new interface

--- src/Model/Java2Html.java
+++ src/Model/Java2Html.java
@@ -1,21 +1,38 @@
 package Model;
 
+import java.util.ArrayList;
+
 /**
  * Created by duzhong on 17-3-4.
  */
 
-
-
 public final class Java2Html {
+
+    private ArrayList<IResultGetter> _getters;
 
     private Configuration _config;
 
-    Java2Html(Configuration config) {
-        _config = config;
+    public Java2Html() {
+        _getters = new ArrayList<IResultGetter>();
     }
 
-    String convert(String javaCode) {
-        return "";
+    public void convert(String javaCode) {
+        String result = "";
+        for (IResultGetter getter : _getters) {
+            getter.getResult(result);
+        }
+    }
+
+    public void addGetter(IResultGetter getter) {
+        _getters.add(getter);
+    }
+
+    public void removeGetter(IResultGetter getter) {
+        _getters.remove(getter);
+    }
+
+    public void set_config(Configuration _config) {
+        this._config = _config;
     }
 
     public Configuration get_config() {
